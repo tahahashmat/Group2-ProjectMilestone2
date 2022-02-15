5744468e830f61dee95c0d7ea4e48ed766460ec2
Taha Hashmat <taha.hashmat@ontariotechu.net>
2022-02-15 11:48:48 -0500

    venv deleted

--- Taha Hashmat - 100689792/ProjectMilestone2/v1/KafkaPython/consumer.py
+++
@@ -1,7 +0,0 @@
-from kafka import KafkaConsumer
-
-TOPIC_NAME = 'items'
-consumer = KafkaConsumer(TOPIC_NAME)
-
-for message in consumer:
-    print(message)
--- Taha Hashmat - 100689792/ProjectMilestone2/v1/KafkaPython/producer.py
+++
@@ -1,9 +0,0 @@
-from kafka import KafkaProducer
-
-TOPIC_NAME = ''
-KAFKA_SERVER = "localhost:9093"
-
-producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
-
-producer.send(TOPIC_NAME, b'Test Message!!')
-producer.flush()
